1faaa2b9dc58e55fb26f911d0e9f92c3baa6bc55
Steve Dougherty <steve@asksteved.com>
2014-10-13 15:19:40 -0400

    Increment score when collecting items; display it

---
+++ Overnight-at-the-Mall/Assets/Collectable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	public int worth;
+	public string pickUpBy = "Player";
+	private Score score;
+
+	// Use this for initialization
+	void Start () {
+		score = GameObject.Find("ScoreCube").GetComponent<Score>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name.Equals(pickUpBy)) {
+			score.Add(worth);
+			Destroy(this.gameObject);
+		}
+	}
+}
