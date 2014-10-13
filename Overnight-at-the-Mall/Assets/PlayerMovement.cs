2fce2987f579840a257b154ffdf514727760e0b5
Steve Dougherty <steve@asksteved.com>
2014-10-13 09:28:46 -0400

    Initial commit

---
+++ Overnight-at-the-Mall/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Dictionary<KeyCode, Vector3> controls = new Dictionary<KeyCode, Vector3>
+	{
+		{KeyCode.W, new Vector3(0, 1, 0)},
+		{KeyCode.S, new Vector3(0, -1, 0)},
+		{KeyCode.A, new Vector3(-1, 0, 0)},
+		{KeyCode.D, new Vector3(1, 0, 0)},
+	};
+
+	float moveSpeed = 500;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocity = new Vector3();
+		foreach (var item in controls) {
+			if (Input.GetKey(item.Key)) {
+				velocity += item.Value;
+			}
+		}
+
+		velocity.Normalize();
+		this.gameObject.rigidbody.velocity = velocity * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Overnight at the Mall
+
+A bet. A night at the mall. Who knows what you'll do with all that free time?
