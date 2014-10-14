16073baec3ca20c00675636b9e898cb66be2622b
Steve Dougherty <steve@asksteved.com>
2014-10-14 15:20:59 -0400

    Enlarge score and countdown text

--- Overnight-at-the-Mall/Assets/Scripts/CountdownTimer.cs
+++ Overnight-at-the-Mall/Assets/Scripts/CountdownTimer.cs
@@ -12,25 +12,16 @@ public class CountdownTimer : MonoBehaviour
 	private float m_seconds;
 	private float m_mins;
 	private float m_totalmiliseconds;
-	
-	//-----------------------------------------------------------------------------------
-	//Note: If you had a way to take in a username or initials you could 
-	//do something along the lines of the below code instead of just using 
-	//a list
-	//
-	//private Dictionary<string, int> m_scoreDictionary = new Dictionary<string, int>();
-	//
-	//With a dictionary you could link a name to a score value, just a thought :P
-	//For now this script just uses the list set up below.
-	//-----------------------------------------------------------------------------------
-	private List<int> m_scores = new List<int>();
+
+	private GUIStyle style;
 	
 	// Use this for initialization
 	void Start()
 	{	
 		m_startingMinutes = GlobalValues.S.startTime ();
 		this.Init(m_startingMinutes);
-		//m_timerLabel = guiText;
+		style = new GUIStyle();
+		style.richText = true;
 	}
 	
 	// Update is called once per frame
@@ -64,29 +55,13 @@ public class CountdownTimer : MonoBehaviour
 			m_miliseconds = 0.0f;
 			m_seconds = 0.0f;
 			m_mins = 0.0f;
-		}/*
-		
-		if ((int)m_miliseconds > 9)
-		{
-			m_timerLabel.text = string.Format("{0}:{1}:{2}", m_mins, m_seconds, (int)m_miliseconds);
 		}
-		else
-		{
-			m_timerLabel.text = string.Format("{0}:{1}:0{2}", m_mins, m_seconds, (int)m_miliseconds);
-		}*/
-
 	}
 	void OnGUI(){
-		if ((int)m_miliseconds > 9)
-		{
-		//	m_timerLabel.text = string.Format("{0}:{1}:{2}", m_mins, m_seconds, (int)m_miliseconds);
-			GUI.Label(new Rect(Screen.width-50, 0, Screen.width, Screen.height), string.Format("{0}:{1}:{2}", m_mins, m_seconds, (int)m_miliseconds));
-		}
-		else
-		{
-		//	m_timerLabel.text = string.Format("{0}:{1}:0{2}", m_mins, m_seconds, (int)m_miliseconds);
-			GUI.Label(new Rect(Screen.width-50, 0, Screen.width, Screen.height), string.Format("{0}:{1}:0{2}", m_mins, m_seconds, (int)m_miliseconds));
-		}
+		GUI.contentColor = Color.white;
+		GUI.Label(new Rect(Screen.width - 100, 0, Screen.width, Screen.height),
+		          string.Format("<size=20><color=white>{0:d2}:{1:d2}:{2:d2}</color></size>", (int) m_mins, (int) m_seconds, (int)m_miliseconds),
+		          style);
 	}
 	
 	/// <summary>
@@ -109,32 +84,4 @@ public class CountdownTimer : MonoBehaviour
 	{
 		m_startTimer = p_pause;
 	}
-	
-	/// <summary>
-	/// Store the score in a list
-	/// </summary>
-	public void StoreScore()
-	{
-		//Store the total milisceconds left in as the score so it we want to
-		//use it later we have a number we can format back into MM:SS:mm
-		this.m_scores.Add((int)m_totalmiliseconds);
-		
-		//Something to consider here is that these score will not be persistent 
-		//between games, you may want to consider using the PlayerPrefs to store
-		//a string that can be parsed out and fed back into the m_scores list.
-		//link: http://docs.unity3d.com/Documentation/ScriptReference/PlayerPrefs.html
-		
-	}
-
-	/*private Camera perspective;
-	private Camera orthographic;
-	void Awake(){
-		perspective = GameObject.Find ("Main Camera").camera;
-		orthographic = GameObject.Find ("Main Camera").camera;
-	}
-	void LateUpdate()
-	{       
-		var position = perspective.WorldToNormalizedViewportPoint(transform.position);
-		transform.localPosition = orthographic.NormalizedViewportToWorldPoint(position);
-	}*/
 }
